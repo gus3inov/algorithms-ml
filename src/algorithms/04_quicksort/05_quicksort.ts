3f19bb34265e4b7f0f4fe15fa5626b99b9ed4ae5
Muslim <gusejnov1999@list.ru>
2018-08-03 23:25:05 +0300

    chapter-04/05_quicksort delete log

--- src/algorithms/04_quicksort/05_quicksort.ts
+++ src/algorithms/04_quicksort/05_quicksort.ts
@@ -5,12 +5,8 @@ export const quickSort = (array: number[]): number[] => {
     const pivot: number = array[0];
     const keysAreLessPivot: number[] = array.slice(1).filter(key => key <= pivot);
     const keysAreMorePivot: number[] = array.slice(1).filter(key => key > pivot);
-    console.log({
-        pivot: pivot,
-        keysAreLessPivot,
-        keysAreMorePivot,
-    })
+
     return [...quickSort(keysAreLessPivot), pivot, ...quickSort(keysAreMorePivot)];
 };
 
-console.log(quickSort([10, 5, 2, 3]));
+console.log(quickSort([10, 5, 12, 3]));
