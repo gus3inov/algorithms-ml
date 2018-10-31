cf1e4435e9a1e0222ebefeea2fe9823ac371bd8c
Muslim <gusejnov1999@list.ru>
2018-10-31 23:40:56 +0300

    update dijkstras algorithm

--- src/algorithms/07_dijkstras_algorithm/01_dijkstras_algorithm.ts
+++ src/algorithms/07_dijkstras_algorithm/01_dijkstras_algorithm.ts
@@ -1,78 +1,73 @@
 // the graph
-
-type Graph = {
-  start: {
-    a: number;
-    b: number;
-  } | object;
-  a: number;
-  b: number;
-  fin: object;
-}
-
-const graph: Graph | object = {};
-graph.start.a = 6;
-graph.start.b = 2;
-
-graph.a.fin = 1;
-
-graph.b.a = 3;
-graph.b.fin = 5;
-
+const graph: any = {
+    start: {
+        a: 6,
+        b: 2,
+    },
+    a: {
+        fin: 1,
+    },
+    b: {
+        a: 3,
+        fin: 5,
+    },
+    fin: {}
+};
 
 // The costs table
-const costs = {};
-costs.a = 6;
-costs.b = 2;
-costs.fin = Infinity;
+const costs: any = {
+    a: 6,
+    b: 2,
+    fin: Infinity
+};
 
 // the parents table
-const parents = {};
-parents.a = 'start';
-parents.b = 'start';
-parents.fin = null;
+const parents: any = {
+    a: 'start',
+    b: 'start',
+    fin: null
+};
 
 let processed = [];
 
-
 const findLowestCostNode = (itCosts) => {
-  let lowestCost = Infinity;
-  let lowestCostNode = null;
+    let lowestCost = Infinity;
+    let lowestCostNode = null;
 
-  Object.keys(itCosts).forEach((node) => {
-    const cost = itCosts[node];
-    // If it's the lowest cost so far and hasn't been processed yet...
-    if (cost < lowestCost && (processed.indexOf(node) === -1)) {
-      // ... set it as the new lowest-cost node.
-      lowestCost = cost;
-      lowestCostNode = node;
-    }
-  });
-  return lowestCostNode;
+    Object.keys(itCosts).forEach((node) => {
+        const cost = itCosts[node];
+        // If it's the lowest cost so far and hasn't been processed yet...
+        if (cost < lowestCost && (processed.indexOf(node) === -1)) {
+            // ... set it as the new lowest-cost node.
+            lowestCost = cost;
+            lowestCostNode = node;
+        }
+    });
+    return lowestCostNode;
 };
 
 let node = findLowestCostNode(costs);
 
 while (node !== null) {
-  const cost = costs[node];
-  // Go through all the neighbors of this node
-  const neighbors = graph[node];
-  Object.keys(neighbors).forEach((n) => {
-    const newCost = cost + neighbors[n];
-    // If it's cheaper to get to this neighbor by going through this node
-    if (costs[n] > newCost) {
-      // ... update the cost for this node
-      costs[n] = newCost;
-      // This node becomes the new parent for this neighbor.
-      parents[n] = node;
-    }
-  });
+    const cost = costs[node];
+    // Go through all the neighbors of this node
+    const neighbors = graph[node];
+    Object.keys(neighbors).forEach((n) => {
+        const newCost = cost + neighbors[n];
+        // If it's cheaper to get to this neighbor by going through this node
+        if (costs[n] > newCost) {
+            // ... update the cost for this node
+            costs[n] = newCost;
+            // This node becomes the new parent for this neighbor.
+            parents[n] = node;
+        }
+    });
 
-  // Mark the node as processed
-  processed = processed.concat(node);
+    // Mark the node as processed
+    processed = processed.concat(node);
 
-  // Find the next node to process, and loop
-  node = findLowestCostNode(costs);
+    // Find the next node to process, and loop
+    node = findLowestCostNode(costs);
 }
 
 console.log('Cost from the start to each node:');
