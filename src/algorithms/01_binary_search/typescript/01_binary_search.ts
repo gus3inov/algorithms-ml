8dee01db6f0dfa5f1586df2964e002605751d56c
Muslim <gusejnov1999@list.ru>
2019-02-09 21:22:01 +0300

    remove logs

--- src/algorithms/01_binary_search/typescript/01_binary_search.ts
+++ src/algorithms/01_binary_search/typescript/01_binary_search.ts
@@ -1,25 +1,19 @@
 console.clear();
-
 console.time('binary_search');
 
-export const binary_search = <T extends number>(list: Array[T], item: T): T => {
-    let low: T = 0,
-        high: T = list.length - 1;
+export const binary_search = <T>(list: Array<T>, item: T): T => {
+    let low = 0,
+        high = list.length - 1;
 
     while(low <= high) {
-        let mid: T = Math.floor(((low + high) / 2)),
-            guess: T = list[mid];
-        console.log('mid ----', mid);
-        console.log('guess ----', guess);
+        let mid = Math.floor(((low + high) / 2)),
+            guess = list[mid];
 
         if (guess === item) {
-            console.log('end');
             return mid;
         } else if (guess > item) {
-            console.log(`more then ${item}`);
             high = mid - 1;
         } else {
-            console.log(`less then ${item}`);
             low = mid + 1;
         }
     }
@@ -27,8 +21,7 @@ export const binary_search = <T extends number>(list: Array[T], item: T): T => {
     return null;
 };
 
-export const myList: Array[number] = [1, 3, 4, 5, 6, 12];
+export const myList: Array<number> = [1, 3, 4, 5, 6, 12];
 
 console.log(binary_search<number>(myList, 5));
-
 console.timeEnd('binary_search');
